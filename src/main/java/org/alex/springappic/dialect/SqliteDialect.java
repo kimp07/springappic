8b8f17c862c3daa061eb9a8e9d605315cef9750c
kimp07 <kimp07@gmail.com>
2021-01-07 19:52:28 +0200

    Adding Thymeleaf support

--- src/main/java/org/alex/springappic/dialect/SqliteDialect.java
+++ src/main/java/org/alex/springappic/dialect/SqliteDialect.java
@@ -62,47 +62,58 @@ public class SqliteDialect extends Dialect {
         return false;
     }
 
+    @Override
     public boolean supportsCurrentTimestampSelection() {
         return true;
     }
 
+    @Override
     public boolean isCurrentTimestampSelectStringCallable() {
         return false;
     }
 
+    @Override
     public String getCurrentTimestampSelectString() {
         return "select current_timestamp";
     }
 
+    @Override
     public boolean supportsUnionAll() {
         return true;
     }
 
+    @Override
     public boolean hasAlterTable() {
         return false; // As specify in NHibernate dialect
     }
 
+    @Override
     public boolean dropConstraints() {
         return false;
     }
 
+    @Override
     public String getAddColumnString() {
         return "add column";
     }
 
+    @Override
     public String getForUpdateString() {
         return "";
     }
 
+    @Override
     public boolean supportsOuterJoinForUpdate() {
         return false;
     }
 
+    @Override
     public String getDropForeignKeyString() {
         throw new UnsupportedOperationException(
                 "No drop foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddForeignKeyConstraintString(
             String constraintName, String[] foreignKey, String referencedTable,
             String[] primaryKey, boolean referencesPrimaryKey) {
@@ -110,15 +121,18 @@ public class SqliteDialect extends Dialect {
                 "No add foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddPrimaryKeyConstraintString(String constraintName) {
         throw new UnsupportedOperationException(
                 "No add primary key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public boolean supportsIfExistsBeforeTableName() {
         return true;
     }
 
+    @Override
     public boolean supportsCascadeDelete() {
         return false;
     }
